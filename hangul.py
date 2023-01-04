7276f9969eb1e9ea2c3ff51c6a67f87bafaad369
leejm1229 <102233945+leejm1229@users.noreply.github.com>
2023-01-04 15:13:08 +0900

    Update hangul.py

--- hangul.py
+++ hangul.py
@@ -1,5 +1,15 @@
-# matplotlib 그래프 한글 깨짐 방지
-!pip install matplotlib -U
-!sudo apt-get install -y fonts-nanum 
-!sudo fc-cache -fv 
-!rm ~/.cache/matplotlib -rfhttps://github.com/jeongmin1229/etc/blob/main/hangul.py
+import matplotlib
+from matplotlib import font_manager, rc
+import platform
+
+try : 
+    if platform.system() == 'Windows':
+    # 윈도우인 경우
+        font_name = font_manager.FontProperties(fname="c:/Windows/Fonts/malgun.ttf").get_name()
+        rc('font', family=font_name)
+    else:    
+    # Mac 인 경우
+        rc('font', family='AppleGothic')
+except : 
+    pass
+matplotlib.rcParams['axes.unicode_minus'] = False  
